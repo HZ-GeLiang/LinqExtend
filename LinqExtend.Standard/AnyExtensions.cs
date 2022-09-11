3a77e7d4139325105730ab13082864ed90ae75cd
GeLiang <childliang@hotmail.com>
2022-09-11 22:27:20 +0800

    新增扩展方法

---
+++ LinqExtend.Standard/AnyExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LinqExtend
+{
+    public static class AnyExtensions
+    {
+        public static bool Any(this ICollection source) => source != null && source.Count > 0;
+        public static bool Any(this StringBuilder source) => source != null && source.Length > 0;
+        public static bool Any(this string source) => source != null && source.Length > 0;
+
+    }
+}
