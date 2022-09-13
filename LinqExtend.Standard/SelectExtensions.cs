e3a161fdda9c7df6970c100b29819d3bc52fa9e3
GeLiang <childliang@hotmail.com>
2022-09-13 21:45:30 +0800

    新增SelectMap(v0.1)

--- LinqExtend.Standard/SelectExtensions.cs
+++ LinqExtend.Standard/SelectExtensions.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using LinqExtend.ExtendMethods;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
 using System.Text;
 
 namespace LinqExtend
@@ -43,5 +46,165 @@ namespace LinqExtend
             return list;
         }
 
+        public static IEnumerable<TResult> SelectMap<TSource, TResult>(this IEnumerable<TSource> source)
+            where TSource : class
+            where TResult : class, new()
+        => SelectMap<TSource, TResult>(source, null);
+
+        private static IEnumerable<TResult> SelectMap<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> selector)
+            where TSource : class
+            where TResult : class, new()
+        {
+            #region 辅助方法
+            bool IsNullableType(Type type) => type.GetTypeInfo().IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>);
+
+            Type GetNullableTType(Type type) => type.GetProperty("Value").PropertyType;
+
+#if DEBUG
+            //Debug下,  如果转换异常,可以知道是哪个属性
+            object ChangeType(PropertyInfo propertyInfo, object val, Type type) => Convert.ChangeType(val, IsNullableType(type) ? GetNullableTType(type) : type);
+#else
+            object ChangeType(object val, Type type) => Convert.ChangeType(val, IsNullableType(type) ? GetNullableTType(type) : type);
+#endif
+
+            //是否为值类型
+            bool IsStructType(Type type)
+            {
+#if NET6_0_OR_GREATER
+
+                ArgumentNullException.ThrowIfNull(type);
+#else
+                if (type == null)
+                {
+                    throw new ArgumentNullException(nameof(type));
+                }
+#endif
+                var isStructType = !type.IsClass;
+                if (isStructType)
+                {
+                    return true;
+                }
+
+                if (IsNullableType(type) && !GetNullableTType(type).IsClass)
+                {
+                    return true;
+                }
+                return false;
+            }
+
+            bool CanSetPropertyValue(PropertyInfo propertyInfo)
+            {
+                //if (!propertyInfo.CanWrite)// 判断此属性是否有Setter
+                //{
+                //    return false; ;//该属性不可写，直接跳出
+                //}
+
+                // If not writable then cannot null it;
+                // if not readable then cannot check it's value
+                // 后面2个表示:  Get and set methods have to be public
+                if (!propertyInfo.CanWrite ||
+                    !propertyInfo.CanRead ||
+                    propertyInfo.GetGetMethod(false) == null ||
+                    propertyInfo.GetSetMethod(false) == null
+                    )
+                {
+                    return false;
+                }
+                return true;
+            }
+            #endregion
+
+            if (source == null)
+            {
+                return Enumerable.Empty<TResult>();
+            }
+
+            //获得 TSource 和 TResult 共有的属性
+            var props = typeof(TSource).GetProperties().ToHashSet(a => a.Name);
+            props.IntersectWith(typeof(TResult).GetProperties().ToHashSet(a => a.Name));
+
+            var parameterExp = Expression.Parameter(typeof(TSource), "a");
+
+            MemberBinding[] bindings = new MemberBinding[props.Count];
+
+            var i = 0;
+            foreach (var propertyName in props)
+            {
+                //if (selector != null)
+                //{
+                //   todo: 计划支持,部分属性按配置的来, 没有配置的按约定的走
+                //}
+
+                //todo: 计划支持类型不一致时的情况: ToList<T> 的实现参考...
+
+                // if (propertyInfo.PropertyType.IsEnum)
+                // {
+                //     propertyInfo.SetValue(model, Enum.ToObject(propertyInfo.PropertyType, colValue));
+                //     continue;
+                // }
+                // if (propertyInfo.DeclaringType == colValue.GetType())
+                // {
+                //     propertyInfo.SetValue(model, colValue);
+                //     continue;
+                // }
+                // if (propertyInfo.PropertyType == typeof(string))
+                // {
+                //     propertyInfo.SetValue(model, colValue.ToString());
+                //     continue;
+                // }
+
+                // else //ChangeType 转换失败会异常
+                // { 
+                //                        
+                //#if DEBUG
+                //      var new_colValue = ChangeType(propertyInfo, colValue, propertyInfo.PropertyType);
+                //#else
+                //      var new_colValue = ChangeType(colValue, propertyInfo.PropertyType);
+                //#endif
+                //      propertyInfo.SetValue(model, new_colValue);
+                // }
+
+
+                bindings[i++] =
+                    Expression.Bind(
+                        typeof(TResult).GetProperty(propertyName),   //  TResult 的 set_UserNickName()
+                        Expression.Property(parameterExp, propertyName)// TSource 的 a.UserNickName
+                    );
+            }
+
+            MemberInitExpression memberInitExpression =
+                Expression.MemberInit(
+                    Expression.New(typeof(TResult)),
+                    bindings
+                );
+
+            Type genericType_arg2 = typeof(TResult);
+            Type genericType = typeof(Func<,>);
+            Type[] templateTypeSet = new[] { typeof(TSource), genericType_arg2 };
+            Type implementType = genericType.MakeGenericType(templateTypeSet);
+
+            var lambda =
+                Expression.Lambda(
+                    implementType,
+                    memberInitExpression,
+                    new ParameterExpression[1] { parameterExp }
+                );
+
+            var methodPara = new object[] { source, lambda.Compile() };
+
+            var SelectMehtod =
+                    typeof(System.Linq.Enumerable)
+                    .GetMethods(BindingFlags.Public | BindingFlags.Static)
+                    .First(mi => mi.Name == "Select"
+                        && mi.GetParameters().Length == 2
+                        && mi.GetParameters().Last().ParameterType.GenericTypeArguments.Length == 2);
+
+            IEnumerable<TResult> list = (IEnumerable<TResult>)
+                SelectMehtod.MakeGenericMethod(
+                    new Type[] { typeof(TSource), typeof(TResult) }
+                ).Invoke(null, methodPara);
+
+            return list;
+        }
     }
 }
--- LinqExtend.Test/SelectExtensionsTest.cs
+++ LinqExtend.Test/SelectExtensionsTest.cs
@@ -1,4 +1,7 @@
+using System;
 using System.Data;
+using System.Linq;
+using System.Linq.Expressions;
 
 namespace LinqExtend.Test
 {
@@ -18,6 +21,78 @@ namespace LinqExtend.Test
 
             Assert.AreEqual(1, ids.First());
             Assert.AreEqual(2, ids.Last());
+        }
+
+        public class People
+        {
+            public int Id { get; set; }
+            public int Age { get; set; }
+            public string Name { get; set; }
+        }
+
+        public class PeopleDto
+        {
+            public int Id { get; set; }
+            public int Age { get; set; }
+        }
+
+        [TestMethod]
+        public void SelectMap()
+        {
+            List<People> peoples = new List<People>()
+            {
+                new People (){ Id =1 , Age =1 , Name="1"},
+                new People (){ Id =2 , Age =2 , Name="2"},
+            };
+
+            var list2 = peoples.SelectMap<People, PeopleDto>();
+
+            Assert.AreEqual(2, list2.Count());
+
+            var list = peoples.Select(a => new PeopleDto
+            {
+                Id = a.Id,
+                Age = a.Age,
+            });
+
+            Expression<Func<People, PeopleDto>> selector = a => new PeopleDto
+            {
+                Id = a.Id,
+                Age = a.Age,
+            };
+
+
+            //IEnumerable<PeopleDto> list = Enumerable.Select(peoples, delegate (People a)
+            //{
+            //    PeopleDto peopleDto = new PeopleDto();
+            //    peopleDto.Id = a.Id;
+            //    peopleDto.Age = a.Age;
+            //    return peopleDto;
+            //});
+
+            //ParameterExpression parameterExpression = Expression.Parameter(typeof(People), "a");
+            //Expression<Func<People, PeopleDto>> selector =
+            //    Expression.Lambda<Func<People, PeopleDto>>(
+            //        Expression.MemberInit(
+            //            Expression.New(typeof(PeopleDto)),
+            //            --dnspy
+            //        	  Expression.Bind(
+            //                methodof(
+            //                   SelectExtensionsTest.PeopleDto.set_Id(int)),
+            //                   Expression.Property(
+            //                      parameterExpression,
+            //                      methodof(SelectExtensionsTest.People.get_Id()
+            //                   )
+            //               )
+            //            ),
+            //            --ilspy
+            //            Expression.Bind(
+            //                (MethodInfo)MethodBase.GetMethodFromHandle((RuntimeMethodHandle)/*OpCode not supported: LdMemberToken*/),
+            //                Expression.Property(parameterExpression,
+            //                (MethodInfo)MethodBase.GetMethodFromHandle((RuntimeMethodHandle)/*OpCode not supported: LdMemberToken*/)))
+            //        ),
+            //        new ParameterExpression[1] { parameterExpression }
+            //    );
         }
     }
 }
