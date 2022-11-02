1deccc29e0b9dbb5b46c4adf2c931af0f7c5bded
GeLiang <childliang@hotmail.com>
2022-11-02 20:12:09 +0800

    增加倒序排序的支持

---
+++ LinqExtend.Standard/Consts/ConstTypeFullName.cs
@@ -0,0 +1,8 @@
+namespace LinqExtend.Consts
+{
+    internal static partial class ConstTypeFullName
+    {
+        public const string PropertyExpression = "System.Linq.Expressions.PropertyExpression";
+        public const string UnaryExpression = "System.Linq.Expressions.UnaryExpression";
+    }
+}
