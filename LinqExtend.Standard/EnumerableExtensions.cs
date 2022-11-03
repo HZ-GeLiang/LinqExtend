6055139023f13a24510809c8496a71e081601066
GeLiang <gel@ccf.com.cn>
2022-11-03 11:33:23 +0800

    调整csproj和新增一个编译后会被注释的文件

---
+++ LinqExtend.Standard/EnumerableExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LinqExtend
+{
+    public static class EnumerableExtensions
+    {
+#if !NET6_0_OR_GREATER && !NETSTANDARD1_0_OR_GREATER 
+        // 注: net 6.0 自带了 DistinctBy,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <typeparam name="TKey"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="keySelector"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
+        public static IEnumerable<TSource> Distinct<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
+        {
+            if (source is null)
+            {
+                //return Enumerable.Empty<TSource>();
+                throw new ArgumentException($@"{nameof(source)} can not be null");
+            }
+
+            var hashSet = new HashSet<TKey>();
+            foreach (TSource item in source)
+            {
+                if (hashSet.Add(keySelector(item)))
+                {
+                    yield return item;
+                }
+            }
+        }
+
+#endif
+
+    }
+}
