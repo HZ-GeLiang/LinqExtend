74b3e2ca8926fe181f9cbe6925bca727aaf75be2
GeLiang <childliang@hotmail.com>
2022-12-03 15:39:11 +0800

    feat: IsNullOrEmpty

---
+++ LinqExtend.EFTest/EF/Book.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+[Table("T_Books")]
+public class Book
+{
+    [Key]
+    public long Id { get; set; }//主键
+    [MaxLength(50)]
+    [Required]
+    public DateTime PubTime { get; set; }//发布日期
+    public double Price { get; set; }//单价
+    public BookInfo BookInfo { get; set; }  
+    public string? Publisher { get; set; }  
+}
+
+public record BookInfo
+{
+    public string Title { get; set; }//标题
+
+    [MaxLength(20)]
+    [Required]
+    public string AuthorName { get; set; }//作者名字
+}
+
+class BookConfig : IEntityTypeConfiguration<Book>
+{
+    public void Configure(EntityTypeBuilder<Book> builder)
+    {
+        builder.OwnsOne(a => a.BookInfo); // owned entity
+    }
+}
