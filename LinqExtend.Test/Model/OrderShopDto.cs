0974c3f5fde091721aa41574eae63d46bdf95264
GeLiang <childliang@hotmail.com>
2022-12-17 22:42:53 +0800

    添加单元测试

--- LinqExtend.Test/Model/OrderShopDto.cs
+++ LinqExtend.Test/Model/OrderShopDto.cs
@@ -6,9 +6,10 @@ public record class OrderShopDto
 {
     public long Id { get; set; }
     public long ShopId { get; set; }
+    public DateTime PaymentTime { get; set; }
     public string ShopName { get; set; }
     public int UserId { get; set; }
 
-    public DateTime PaymentTime { get; set; }
+    public DateTime PubTime { get; set; }//发布日期
 
 }
