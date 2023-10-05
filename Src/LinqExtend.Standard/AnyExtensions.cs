3ca96b03c782b694ba6add1e5fcf936e89e6e560
GeLiang <childliang@hotmail.com>
2023-10-05 20:10:15 +0800

    调整 Any的扩展

--- Src/LinqExtend.Standard/AnyExtensions.cs
+++ Src/LinqExtend.Standard/AnyExtensions.cs
@@ -8,9 +8,23 @@ namespace LinqExtend
 {
     public static class AnyExtensions
     {
-        public static bool Any(this ICollection source) => source != null && source.Count > 0;
-        public static bool Any(this StringBuilder source) => source != null && source.Length > 0;
-        public static bool Any(this string source) => source != null && source.Length > 0;
+        public static bool AnyEnhance(this ICollection source) => source != null && source.Count > 0;
+        public static bool AnyEnhance(this StringBuilder source) => source != null && source.Length > 0;
+        public static bool AnyEnhance(this string source) => source != null && source.Length > 0;
 
+        /// <summary>
+        /// Any方法的增强
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static bool AnyEnhance<TSource>(this IEnumerable<TSource> source)
+        {
+            if (source == null)
+            {
+                return false;
+            }
+            return source.Any();
+        }
     }
 }
