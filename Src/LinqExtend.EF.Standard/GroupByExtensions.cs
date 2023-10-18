d5b191c2d179b4ff67aebdeaccd3985ffade0287
GeLiang <childliang@hotmail.com>
2023-10-18 13:18:44 +0800

    增加一个重载

--- Src/LinqExtend.EF.Standard/GroupByExtensions.cs
+++ Src/LinqExtend.EF.Standard/GroupByExtensions.cs
@@ -14,6 +14,15 @@ namespace LinqExtend
         {
             var keySelector = ExpressionHelper.SelectMap<TSource, TKey>();
             return source.GroupBy(keySelector);
-        } 
+        }
+
+
+        public static IQueryable<IGrouping<TKey, TSource>> GroupByMap<TSource, TKey>(this IQueryable<TSource> source, TKey _)
+           where TSource : class
+           where TKey : class, new()
+        {
+            var keySelector = ExpressionHelper.SelectMap<TSource, TKey>();
+            return source.GroupBy(keySelector);
+        }
     }
 }
