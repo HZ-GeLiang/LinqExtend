51aec5a4751710a71573be8dfc0ee1b3498a7088
GeLiang <childliang@hotmail.com>
2023-12-27 14:40:57 +0800

    扩展 AggregateExtensions

--- Src/LinqExtend.Standard/AggregateExtensions.cs
+++ Src/LinqExtend.Standard/AggregateExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LinqExtend.ExtensionMethod;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
@@ -6,48 +7,21 @@ using System.Text;
 
 namespace LinqExtend
 {
+    /// <summary>
+    /// 聚合为一个字符串
+    /// </summary>
     public static class AggregateExtensions
     {
-        #region private Extension Methods
-        private static bool EndWith(this StringBuilder value, string str)
-        {
-            if (value is null) throw new System.ArgumentNullException(nameof(value));
-            if (str is null) throw new System.ArgumentNullException(nameof(str));
-            for (int i = str.Length; i > 0; i--)
-            {
-                if (str[i - 1] != value[value.Length - (str.Length - i + 1)])
-                {
-                    return false;
-                }
-            }
-            return true;
-        }
-
-        private static StringBuilder RemoveLastChar(this StringBuilder value, string str)
-        {
-            if (value is null) throw new System.ArgumentNullException(nameof(value));
-            if (str is null || str.Length <= 0) return value; //不能进行移除操作
-            if (str.Length > value.Length) return value; //移除失败,无法移除
-            if (!value.EndWith(str)) return value;   //EndWith不匹配,无法移除
-
-            return value.Replace(str, string.Empty, value.Length - str.Length, str.Length);
-        }
-
-        #endregion
-
         public static string AggregateToString(this DataRowCollection source, string columnName, string separator)
         {
             if (source is null) throw new ArgumentNullException($"{nameof(source)}参数不能为空", nameof(source));
-            if (!source.AnyEnhance())
-            {
-                return string.Empty;
-            }
-            if (string.IsNullOrEmpty(columnName)) throw new ArgumentNullException($"{nameof(columnName)}参数不能为空", nameof(columnName));
+            if (columnName.HasValue() == false) throw new ArgumentNullException($"{nameof(columnName)}参数不能为空", nameof(columnName));
+            if (separator is null) throw new ArgumentNullException($"{nameof(separator)}参数不能为空", nameof(separator));
+            if (source.Count <= 0) return string.Empty;
 
             var sb = new StringBuilder();
-            var separatorhasValue = !string.IsNullOrEmpty(separator);
 
-            if (separatorhasValue)
+            if (separator.HasValue())
             {
                 foreach (DataRow item in source)
                 {
@@ -62,7 +36,7 @@ namespace LinqExtend
                 }
             }
 
-            if (separatorhasValue)
+            if (separator.HasValue())
             {
                 sb.Replace(separator, string.Empty, sb.Length - separator.Length, separator.Length);
             }
@@ -73,16 +47,13 @@ namespace LinqExtend
         public static string AggregateToString(this DataRowCollection source, Func<DataRow, dynamic> content, string separator)
         {
             if (source is null) throw new ArgumentNullException($"{nameof(source)}参数不能为空", nameof(source));
-            if (!source.AnyEnhance())
-            {
-                return string.Empty;
-            }
             if (content is null) throw new ArgumentNullException($"{nameof(content)}参数不能为空", nameof(content));
+            if (separator is null) throw new ArgumentNullException($"{nameof(separator)}参数不能为空", nameof(separator));
+            if (source.Count <= 0) return string.Empty;
 
             var sb = new StringBuilder();
-            var separatorhasValue = !string.IsNullOrEmpty(separator);
 
-            if (separatorhasValue)
+            if (separator.HasValue())
             {
                 foreach (DataRow row in source)
                 {
@@ -97,7 +68,7 @@ namespace LinqExtend
                 }
             }
 
-            if (separatorhasValue)
+            if (separator.HasValue())
             {
                 sb.Replace(separator, string.Empty, sb.Length - separator.Length, separator.Length);
             }
@@ -105,52 +76,52 @@ namespace LinqExtend
             return txt;
         }
 
-        public static string AggregateToString<TSource>(this IEnumerable<TSource> source, string separator)
+        #region 常见的列表对象调用 AggregateToString, 表现形式一般为 list.AggregateToString(a => a,",")
+
+        public static string AggregateToString<TKey, TValue>(this Dictionary<TKey, TValue>.KeyCollection keys, string separator)
         {
-            if (source is null) throw new ArgumentNullException($"{nameof(source)}参数不能为空", nameof(source));
-            if (!source.Any())
+            var keyType = typeof(TKey);
+            if (keyType.IsClass == false || keyType == typeof(string))
             {
-                return string.Empty;
+                var list = new List<string>(keys.Count);
+                return keys.ToList().AggregateToString(a => a, separator);
             }
-
-            var sb = new StringBuilder();
-            var separatorhasValue = !string.IsNullOrEmpty(separator);
-
-            if (separatorhasValue)
-            {
-                foreach (var item in source)
-                {
-                    sb.Append(item).Append(separator);
-                }
-            }
-            else
-            {
-                foreach (var item in source)
-                {
-                    sb.Append(item);
-                }
-            }
-            if (sb.Length <= 0)
-            {
-                return string.Empty;
-            }
-
-            if (separatorhasValue)
-            {
-                sb.Replace(separator, string.Empty, sb.Length - separator.Length, separator.Length);
-            }
-            var txt = sb.ToString();
-            return txt;
+            throw new Exception($"不支持的TKey类型:{keyType}");
         }
+
+        public static string AggregateToString(this List<string> source, string separator)
+        {
+            return source.Where(a => string.IsNullOrEmpty(a) == false).AggregateToString(a => a, separator);
+        }
+        public static string AggregateToString(this IOrderedEnumerable<string> source, string separator)
+        {
+            return source.Where(a => string.IsNullOrEmpty(a) == false).AggregateToString(a => a, separator);
+        }
+        public static string AggregateToString(this List<int> source, string separator)
+        {
+            return source.AggregateToString(a => a, separator);
+        }
+        public static string AggregateToString(this List<int?> source, string separator)
+        {
+            return source.Where(a => a.HasValue).AggregateToString(a => a, separator);
+        }
+
+        #endregion
 
         public static string AggregateToString<TSource>(this IEnumerable<TSource> source, Func<TSource, TSource> content, string separator)
         {
             if (source is null) throw new ArgumentNullException($"{nameof(source)}参数不能为空", nameof(source));
-            if (!source.Any())
-            {
-                return string.Empty;
-            }
             if (content is null) throw new ArgumentNullException($"{nameof(content)}参数不能为空", nameof(content));
+            if (separator is null) throw new ArgumentNullException($"{nameof(separator)}参数不能为空", nameof(separator));
+            if (source.Any() == false) return string.Empty;
+
+#if DEBUG
+            if (source.GetType() == typeof(string)) // string is IEnumerable<TSource> == true
+            {
+                // 一般来说这个场景不存在,防止代码无意中写错, 这个方法不支持 string 调用
+                throw new Exception("字符串类型的变量不允许调用该方法");
+            }
+#endif
 
             var sb = new StringBuilder();
             var separatorhasValue = !string.IsNullOrEmpty(separator);
@@ -184,15 +155,27 @@ namespace LinqExtend
             return txt;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="content">dynamic的目的: 调用端不用刻意的使用 ToString() </param>
+        /// <param name="separator"></param>
         public static string AggregateToString<TSource>(this IEnumerable<TSource> source, Func<TSource, dynamic> content, string separator)
         {
             if (source is null) throw new ArgumentNullException($"{nameof(source)}参数不能为空", nameof(source));
-            if (!source.Any())
-            {
-                return string.Empty;
-            }
             if (content is null) throw new ArgumentNullException($"{nameof(content)}参数不能为空", nameof(content));
+            if (separator is null) throw new ArgumentNullException($"{nameof(separator)}参数不能为空", nameof(separator));
+            if (source.Any() == false) return string.Empty;
 
+#if DEBUG
+            if (source.GetType() == typeof(string)) // string is IEnumerable<TSource> == true
+            {
+                // 一般来说这个场景不存在,防止代码无意中写错, 这个方法不支持 string 调用
+                throw new Exception("字符串类型的变量不允许调用该方法");
+            }
+#endif
 
             var sb = new StringBuilder();
             var separatorhasValue = !string.IsNullOrEmpty(separator);
---
+++ Src/LinqExtend.Standard/ExtensionMethod/StringExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinqExtend.ExtensionMethod
+{
+    internal static partial class StringExtensions
+    {
+        /// <summary>
+        /// 是否有值
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static bool HasValue(this string value)
+        {
+            return !string.IsNullOrEmpty(value);
+        }
+    }
+}
