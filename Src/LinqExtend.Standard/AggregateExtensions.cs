abec3f1eaa1b6b71ea72364f5710e48452bcde48
GeLiang <childliang@hotmail.com>
2025-05-18 16:00:42 +0800

    AggregateToString 增加 predicate

--- Src/LinqExtend.Standard/AggregateExtensions.cs
+++ Src/LinqExtend.Standard/AggregateExtensions.cs
@@ -9,15 +9,88 @@ namespace LinqExtend;
 /// </summary>
 public static class AggregateExtensions
 {
+    #region 常见的列表对象调用 AggregateToString, 表现形式一般为 list.AggregateToString(a => a,",")
+
+    public static string AggregateToString<TKey, TValue>(this Dictionary<TKey, TValue>.KeyCollection keys, string separator)
+    {
+        var keyType = typeof(TKey);
+        if (keyType.IsClass == false || keyType == typeof(string))
+        {
+            return keys.ToList().AggregateToString(null, a => a, separator);
+        }
+        throw new Exception($"不支持的TKey类型:{keyType}");
+    }
+
+    public static string AggregateToString<TKey, TValue>(this Dictionary<TKey, TValue>.KeyCollection keys, Func<TKey, bool> predicate, string separator)
+    {
+        var keyType = typeof(TKey);
+        if (keyType.IsClass == false || keyType == typeof(string))
+        {
+            return keys.ToList().AggregateToString(predicate, a => a, separator);
+        }
+        throw new Exception($"不支持的TKey类型:{keyType}");
+    }
+
+
+    public static string AggregateToString(this List<string> source, string separator)
+    {
+        return source.Where(a => string.IsNullOrEmpty(a) == false).AggregateToString(a => a, separator);
+    }
+
+    public static string AggregateToString(this List<string> source, Func<string, bool> predicate, string separator)
+    {
+        return source.Where(a => string.IsNullOrEmpty(a) == false && predicate.Invoke(a)).AggregateToString(a => a, separator);
+    }
+
+    public static string AggregateToString(this IOrderedEnumerable<string> source, string separator)
+    {
+        return source.Where(a => string.IsNullOrEmpty(a) == false).AggregateToString(a => a, separator);
+    }
+
+    public static string AggregateToString(this IOrderedEnumerable<string> source, Func<string, bool> predicate, string separator)
+    {
+        return source.Where(a => string.IsNullOrEmpty(a) == false && predicate.Invoke(a)).AggregateToString(a => a, separator);
+    }
+
+    public static string AggregateToString(this List<int> source, string separator)
+    {
+        return source.AggregateToString(null, a => a, separator);
+    }
+
+    public static string AggregateToString(this List<int> source, Func<int, bool> predicate, string separator)
+    {
+        return source.AggregateToString(predicate, a => a, separator);
+    }
+
+    public static string AggregateToString(this List<int?> source, string separator)
+    {
+        return source.AggregateToString(a => a.HasValue, a => a, separator);
+    }
+
+    public static string AggregateToString(this List<int?> source, Func<int?, bool> predicate, string separator)
+    {
+        return source.AggregateToString(a => a.HasValue && predicate.Invoke(a), a => a, separator);
+    }
+
+    #endregion
+
+    #region AggregateToString , 聚合为一个字符串
+
+    public static string AggregateToString(this DataRowCollection source, string columnName, string separator)
+    {
+        return AggregateToString(source, null, columnName, separator);
+    }
+
     /// <summary>
     /// 聚合为一个字符串
     /// </summary>
     /// <param name="source"></param>
+    /// <param name="predicate"></param>
     /// <param name="columnName"></param>
     /// <param name="separator"></param>
     /// <returns>没有值时返回""</returns>
     /// <exception cref="ArgumentNullException"></exception>
-    public static string AggregateToString(this DataRowCollection source, string columnName, string separator)
+    public static string AggregateToString(this DataRowCollection source, Func<DataRow, bool> predicate, string columnName, string separator)
     {
         if (source is null)
         {
@@ -38,18 +111,46 @@ public static class AggregateExtensions
 
         var sb = new StringBuilder();
 
+
         if (separator.HasValue())
         {
-            foreach (DataRow item in source)
+            if (predicate == null)
             {
-                sb.Append(item[columnName]).Append(separator);
+                foreach (DataRow item in source)
+                {
+                    sb.Append(item[columnName]).Append(separator);
+                }
             }
+            else
+            {
+                foreach (DataRow item in source)
+                {
+                    if (predicate.Invoke(item))
+                    {
+                        sb.Append(item[columnName]).Append(separator);
+                    }
+                }
+            }
+
         }
         else
         {
-            foreach (DataRow item in source)
+            if (predicate == null)
             {
-                sb.Append(item[columnName]);
+                foreach (DataRow item in source)
+                {
+                    sb.Append(item[columnName]);
+                }
+            }
+            else
+            {
+                foreach (DataRow item in source)
+                {
+                    if (predicate.Invoke(item))
+                    {
+                        sb.Append(item[columnName]);
+                    }
+                }
             }
         }
 
@@ -62,6 +163,11 @@ public static class AggregateExtensions
     }
 
     public static string AggregateToString(this DataRowCollection source, Func<DataRow, dynamic> content, string separator)
+    {
+        return AggregateToString(source, null, content, separator);
+    }
+
+    public static string AggregateToString(this DataRowCollection source, Func<DataRow, bool> predicate, Func<DataRow, dynamic> content, string separator)
     {
         if (source is null)
         {
@@ -82,18 +188,45 @@ public static class AggregateExtensions
 
         var sb = new StringBuilder();
 
+
         if (separator.HasValue())
         {
-            foreach (DataRow row in source)
+            if (predicate == null)
             {
-                sb.Append(content.Invoke(row)).Append(separator);
+                foreach (DataRow row in source)
+                {
+                    sb.Append(content.Invoke(row)).Append(separator);
+                }
+            }
+            else
+            {
+                foreach (DataRow row in source)
+                {
+                    if (predicate.Invoke(row) == true)
+                    {
+                        sb.Append(content.Invoke(row)).Append(separator);
+                    }
+                }
             }
         }
         else
         {
-            foreach (DataRow row in source)
+            if (predicate == null)
             {
-                sb.Append(content.Invoke(row));
+                foreach (DataRow row in source)
+                {
+                    sb.Append(content.Invoke(row));
+                }
+            }
+            else
+            {
+                foreach (DataRow row in source)
+                {
+                    if (predicate.Invoke(row) == true)
+                    {
+                        sb.Append(content.Invoke(row));
+                    }
+                }
             }
         }
 
@@ -105,98 +238,17 @@ public static class AggregateExtensions
         return txt;
     }
 
-    #region 常见的列表对象调用 AggregateToString, 表现形式一般为 list.AggregateToString(a => a,",")
 
-    public static string AggregateToString<TKey, TValue>(this Dictionary<TKey, TValue>.KeyCollection keys, string separator)
-    {
-        var keyType = typeof(TKey);
-        if (keyType.IsClass == false || keyType == typeof(string))
-        {
-            var list = new List<string>(keys.Count);
-            return keys.ToList().AggregateToString(a => a, separator);
-        }
-        throw new Exception($"不支持的TKey类型:{keyType}");
-    }
-
-    public static string AggregateToString(this List<string> source, string separator)
-    {
-        return source.Where(a => string.IsNullOrEmpty(a) == false).AggregateToString(a => a, separator);
-    }
-
-    public static string AggregateToString(this IOrderedEnumerable<string> source, string separator)
-    {
-        return source.Where(a => string.IsNullOrEmpty(a) == false).AggregateToString(a => a, separator);
-    }
-
-    public static string AggregateToString(this List<int> source, string separator)
-    {
-        return source.AggregateToString(a => a, separator);
-    }
-
-    public static string AggregateToString(this List<int?> source, string separator)
-    {
-        return source.Where(a => a.HasValue).AggregateToString(a => a, separator);
-    }
-
-    #endregion
-
+    /// <inheritdoc cref="AggregateToString{TSource}(IEnumerable{TSource}, Func{TSource, bool}, Func{TSource, TSource}, string, int, string)"/>
     public static string AggregateToString<TSource>(this IEnumerable<TSource> source, Func<TSource, TSource> content, string separator)
     {
-        if (source is null)
-        {
-            throw new ArgumentNullException($"{nameof(source)}参数不能为空", nameof(source));
-        }
-        if (content is null)
-        {
-            throw new ArgumentNullException($"{nameof(content)}参数不能为空", nameof(content));
-        }
-        if (separator is null)
-        {
-            throw new ArgumentNullException($"{nameof(separator)}参数不能为空", nameof(separator));
-        }
-        if (source.Any() == false)
-        {
-            return string.Empty;
-        }
+        return AggregateToString(source, null, content, separator, 0, string.Empty);
+    }
 
-#if DEBUG
-        if (source.GetType() == typeof(string)) // string is IEnumerable<TSource> == true
-        {
-            // 一般来说这个场景不存在,防止代码无意中写错, 这个方法不支持 string 调用
-            throw new Exception("字符串类型的变量不允许调用该方法");
-        }
-#endif
-
-        var sb = new StringBuilder();
-        var separatorhasValue = !string.IsNullOrEmpty(separator);
-
-        if (separatorhasValue)
-        {
-            foreach (var item in source)
-            {
-                sb.Append(content.Invoke(item)).Append(separator);
-            }
-        }
-        else
-        {
-            foreach (var item in source)
-            {
-                sb.Append(content.Invoke(item));
-            }
-        }
-
-        if (sb.Length <= 0)
-        {
-            return string.Empty;
-        }
-
-        if (separatorhasValue)
-        {
-            sb.Replace(separator, string.Empty, sb.Length - separator.Length, separator.Length);
-        }
-
-        var txt = sb.ToString();
-        return txt;
+    /// <inheritdoc cref="AggregateToString{TSource}(IEnumerable{TSource}, Func{TSource, bool}, Func{TSource, TSource}, string, int, string)"/>
+    public static string AggregateToString<TSource>(this IEnumerable<TSource> source, Func<TSource, TSource> content, string separator, int groupCount, string groupSeparator)
+    {
+        return AggregateToString(source, null, content, separator, groupCount, groupSeparator);
     }
 
     /// <summary>
@@ -204,12 +256,209 @@ public static class AggregateExtensions
     /// </summary>
     /// <typeparam name="TSource"></typeparam>
     /// <param name="source"></param>
+    /// <param name="predicate"></param>
+    /// <param name="content"></param>
+    /// <param name="separator"></param>
+    /// <param name="groupCount">每个分组的数量</param>
+    /// <param name="groupSeparator">分组的结尾符号</param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentNullException"></exception>
+    /// <exception cref="Exception"></exception>
+    public static string AggregateToString<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate, Func<TSource, TSource> content, string separator, int groupCount, string groupSeparator)
+    {
+        if (source is null)
+        {
+            throw new ArgumentNullException($"{nameof(source)}参数不能为空", nameof(source));
+        }
+        if (content is null)
+        {
+            throw new ArgumentNullException($"{nameof(content)}参数不能为空", nameof(content));
+        }
+        if (separator is null)
+        {
+            throw new ArgumentNullException($"{nameof(separator)}参数不能为空", nameof(separator));
+        }
+        if (source.Any() == false)
+        {
+            return string.Empty;
+        }
+
+#if DEBUG
+        if (source.GetType() == typeof(string)) // string is IEnumerable<TSource> == true
+        {
+            // 一般来说这个场景不存在,防止代码无意中写错, 这个方法不支持 string 调用
+            throw new Exception("字符串类型的变量不允许调用该方法");
+        }
+#endif
+
+        var sb = new StringBuilder();
+        var separatorhasValue = !string.IsNullOrEmpty(separator);
+
+        if (groupCount > 0 && groupSeparator.HasValue())
+        {
+            var count = 0;
+            var appendGroupSeparator = false;
+
+            if (separatorhasValue)
+            {
+                if (predicate == null)
+                {
+                    foreach (var item in source)
+                    {
+                        if (appendGroupSeparator)
+                        {
+                            sb.Append(groupSeparator);
+                            appendGroupSeparator = false;
+                        }
+                        count++;
+
+                        sb.Append(content.Invoke(item)).Append(separator);
+                        if (count > 0 && count % groupCount == 0)
+                        {
+                            appendGroupSeparator = true;
+                        }
+                    }
+                }
+                else
+                {
+                    foreach (var item in source)
+                    {
+                        if (predicate.Invoke(item))
+                        {
+                            if (appendGroupSeparator)
+                            {
+                                sb.Append(groupSeparator);
+                                appendGroupSeparator = false;
+                            }
+                            count++;
+
+                            sb.Append(content.Invoke(item)).Append(separator);
+                            if (count > 0 && count % groupCount == 0)
+                            {
+                                appendGroupSeparator = true;
+                            }
+                        }
+                    }
+                }
+
+            }
+            else
+            {
+                if (predicate == null)
+                {
+                    foreach (var item in source)
+                    {
+                        if (appendGroupSeparator)
+                        {
+                            sb.Append(groupSeparator);
+                            appendGroupSeparator = false;
+                        }
+                        count++;
+                        sb.Append(content.Invoke(item));
+                        if (count > 0 && count % groupCount == 0)
+                        {
+                            appendGroupSeparator = true;
+                        }
+                    }
+                }
+                else
+                {
+                    foreach (var item in source)
+                    {
+                        if (predicate.Invoke(item))
+                        {
+                            if (appendGroupSeparator)
+                            {
+                                sb.Append(groupSeparator);
+                                appendGroupSeparator = false;
+                            }
+                            count++;
+                            sb.Append(content.Invoke(item));
+                            if (count > 0 && count % groupCount == 0)
+                            {
+                                appendGroupSeparator = true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        else
+        {
+            if (separatorhasValue)
+            {
+                if (predicate == null)
+                {
+                    foreach (var item in source)
+                    {
+                        sb.Append(content.Invoke(item)).Append(separator);
+                    }
+                }
+                else
+                {
+                    foreach (var item in source)
+                    {
+                        if (predicate.Invoke(item))
+                        {
+                            sb.Append(content.Invoke(item)).Append(separator);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                if (predicate == null)
+                {
+                    foreach (var item in source)
+                    {
+                        sb.Append(content.Invoke(item));
+                    }
+                }
+                else
+                {
+                    foreach (var item in source)
+                    {
+                        if (predicate.Invoke(item))
+                        {
+                            sb.Append(content.Invoke(item));
+                        }
+                    }
+                }
+            }
+        }
+
+        if (sb.Length <= 0)
+        {
+            return string.Empty;
+        }
+
+        if (separatorhasValue)
+        {
+            sb.Replace(separator, string.Empty, sb.Length - separator.Length, separator.Length);
+        }
+
+        var txt = sb.ToString();
+        return txt;
+    }
+
+
+    public static string AggregateToString<TSource>(this IEnumerable<TSource> source, Func<TSource, dynamic> content, string separator)
+    {
+        return AggregateToString(source, null, content, separator);
+    }
+
+    /// <summary>
+    /// 聚合为一个字符串
+    /// </summary>
+    /// <typeparam name="TSource"></typeparam>
+    /// <param name="source"></param>
+    /// <param name="predicate"></param>
     /// <param name="content">dynamic的目的: 调用端不用刻意的使用 ToString()</param>
     /// <param name="separator"></param>
     /// <returns>没有值时返回""</returns>
     /// <exception cref="ArgumentNullException"></exception>
     /// <exception cref="Exception"></exception>
-    public static string AggregateToString<TSource>(this IEnumerable<TSource> source, Func<TSource, dynamic> content, string separator)
+    public static string AggregateToString<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate, Func<TSource, dynamic> content, string separator)
     {
         if (source is null)
         {
@@ -242,16 +491,43 @@ public static class AggregateExtensions
 
         if (separatorhasValue)
         {
-            foreach (var item in source)
+            if (predicate == null)
             {
-                sb.Append(content.Invoke(item)).Append(separator);
+                foreach (var item in source)
+                {
+                    sb.Append(content.Invoke(item)).Append(separator);
+                }
             }
+            else
+            {
+                foreach (var item in source)
+                {
+                    if (predicate.Invoke(item))
+                    {
+                        sb.Append(content.Invoke(item)).Append(separator);
+                    }
+                }
+            }
+
         }
         else
         {
-            foreach (var item in source)
+            if (predicate == null)
             {
-                sb.Append(content.Invoke(item));
+                foreach (var item in source)
+                {
+                    sb.Append(content.Invoke(item));
+                }
+            }
+            else
+            {
+                foreach (var item in source)
+                {
+                    if (predicate.Invoke(item))
+                    {
+                        sb.Append(content.Invoke(item));
+                    }
+                }
             }
         }
 
@@ -267,4 +543,6 @@ public static class AggregateExtensions
         var txt = sb.ToString();
         return txt;
     }
+
+    #endregion
 }
--- Test/LinqExtend.Test/AggregateExtensionsTest.cs
+++ Test/LinqExtend.Test/AggregateExtensionsTest.cs
@@ -9,16 +9,41 @@
             public string Name { get; set; }
         }
 
+
+        // 测试值类型键（如 int）的筛选和连接功能
+
+        [TestMethod]
+        public void AggregateToString_WithValueTypeKeys_FiltersAndConcatenates()
+        {
+            // Arrange
+            var dictionary = new Dictionary<int, string>
+            {
+                { 1, "one" },
+                { 2, "two" },
+                { 3, "three" },
+                { 4, "four" }
+            };
+            var keys = dictionary.Keys;
+            Func<int, bool> predicate = x => x % 2 == 0; // 筛选偶数
+            string separator = ", ";
+
+            // Act
+            var result = keys.AggregateToString(predicate, separator);
+
+            // Assert
+            Assert.AreEqual("2, 4", result);
+        }
+
         [TestMethod]
         public void AggregateToString()
         {
             {
                 var peopleList = new List<People>()
-                    {
-                        new People {Id = 1},
-                        new People {Id = 2},
-                        new People {Id = 3},
-                    };
+                {
+                    new People {Id = 1},
+                    new People {Id = 2},
+                    new People {Id = 3},
+                };
                 var str1 = peopleList.AggregateToString(a => a.Id, ",");
                 Assert.AreEqual(str1, "1,2,3");
 
@@ -35,11 +60,11 @@
 
             {
                 var peopleList = new List<People>()
-                    {
-                        new People {Name = "1"},
-                        new People {Name = "2"},
-                        new People {Name = "3"},
-                    };
+                {
+                    new People {Name = "1"},
+                    new People {Name = "2"},
+                    new People {Name = "3"},
+                };
                 var str1 = peopleList.AggregateToString(a => $@"'{a.Name}'", ",");
                 Assert.AreEqual(str1, "'1','2','3'");
 
@@ -53,11 +78,11 @@
 
             {
                 var peopleList = new List<People>()
-                    {
-                        new People {Id = 1},
-                        new People {Id = 2},
-                        new People {Id = 3},
-                    };
+                {
+                    new People {Id = 1},
+                    new People {Id = 2},
+                    new People {Id = 3},
+                };
 
                 var str1 = peopleList.AggregateToString(a => $@"{a.Id}", "\r\n");
                 Assert.AreEqual(str1, "1\r\n2\r\n3");
